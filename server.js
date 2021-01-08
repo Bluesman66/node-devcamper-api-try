62b20fa9f1283ae0936c20c3e9996c23e7ce0e85
Alex Paclin <alex.paclin@gmail.com>
2021-01-08 15:24:32 +0500

    create routes

--- server.js
+++ server.js
@@ -5,6 +5,32 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/1v/bootcamps', (req, res, next) => {
+	res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.get('/api/1v/bootcamps/:id', (req, res, next) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
+app.post('/api/1v/bootcamps', (req, res, next) => {
+	res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+});
+
+app.put('/api/1v/bootcamps/:id', (req, res, next) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/1v/bootcamps/:id', (req, res, next) => {
+	res
+		.status(200)
+		.json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
