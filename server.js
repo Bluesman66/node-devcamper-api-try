c8e2dd2b07b173dc79ab69182c60ff4f4478967f
Alex Paclin <alex.paclin@gmail.com>
2021-01-08 16:30:50 +0500

    morgan middleware

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 const bootcamps = require('./routes/bootcamps');
 
@@ -8,7 +8,9 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.use(logger);
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
