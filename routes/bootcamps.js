0e2c350f35e708539caf6d73297597acfe894120
Alex Paclin <alex.paclin@gmail.com>
2021-01-10 21:36:32 +0500

    Populate, reverse populate with virtuals

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const {
-  getBootcamps,
-  getBootcamp,
-  createBootcamp,
-  updateBootcamp,
-  deleteBootcamp,
-  getBootcampsInRadius
+	getBootcamps,
+	getBootcamp,
+	createBootcamp,
+	updateBootcamp,
+	deleteBootcamp,
+	getBootcampsInRadius,
 } = require('../controllers/bootcamps');
 
 // Include other resource routers
@@ -18,15 +18,12 @@ router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
-router
-  .route('/')
-  .get(getBootcamps)
-  .post(createBootcamp);
+router.route('/').get(getBootcamps).post(createBootcamp);
 
 router
-  .route('/:id')
-  .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+	.route('/:id')
+	.get(getBootcamp)
+	.put(updateBootcamp)
+	.delete(deleteBootcamp);
 
 module.exports = router;
