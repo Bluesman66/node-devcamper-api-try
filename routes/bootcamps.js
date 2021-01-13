2dee54b0cd16ab3949d02489984d204599eba69d
Alex Paclin <alex.paclin@gmail.com>
2021-01-13 15:07:27 +0500

    Advanced results middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const {
-  getBootcamps,
-  getBootcamp,
-  createBootcamp,
-  updateBootcamp,
-  deleteBootcamp,
-  getBootcampsInRadius,
-  bootcampPhotoUpload
+	getBootcamps,
+	getBootcamp,
+	createBootcamp,
+	updateBootcamp,
+	deleteBootcamp,
+	getBootcampsInRadius,
+	bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
+
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
 
 // Include other resource routers
 const courseRouter = require('./courses');
@@ -22,14 +25,14 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
 router
-  .route('/')
-  .get(getBootcamps)
-  .post(createBootcamp);
+	.route('/')
+	.get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+	.post(createBootcamp);
 
 router
-  .route('/:id')
-  .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+	.route('/:id')
+	.get(getBootcamp)
+	.put(updateBootcamp)
+	.delete(deleteBootcamp);
 
 module.exports = router;
